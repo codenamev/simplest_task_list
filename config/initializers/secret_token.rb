8002d4f8431feb96abc1b67c4bce610a1c34c6a3
Codename V <thevalentino+code@gmail.com>
2011-08-24 18:07:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnhancedTasks::Application.config.secret_token = '8366120eae955e46fe219dff3457215102964c8a6a7349accbbf23ba216549c353870a14de55add09e228017bfecceda7bb592aa9da98623c28fe3abed68e7af'
